a1d3f213771d644459f633ba0c57d91ae3f2831b
João Vitor Menezes de Jesus <joao_vitor_mj@hotmail.com>
2022-08-14 17:22:52 -0300

    feat/route tests

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link :to="rotaDinamica.name">Serviços</router-link> |
+    <router-link :to="usuarioEditar">Usuário</router-link>
+    <!-- <router-link to="/servicos">Serviços</router-link> -->
+  </nav>
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      rotaDinamica: { name: "servicos" },
+      usuarioEditar: { name: "usuarios-edit", params: { id: 12 } },
+    };
+  },
+  created() {
+    console.log(this.$router.options.routes);
+  },
+};
 </script>
 
 <style>
@@ -21,6 +30,18 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
